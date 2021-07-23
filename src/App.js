8f22d5f37fc9e98b9254e677c5e4a7ffd9466fd3
kobayashi <kortaban@gmail.com>
2021-07-23 16:37:19 +0900

    Update App.js
    rearn state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 
-function App() {
-  return (
-    // <div className="App">hello world</div>
-    <h1 >hello world</h1>
-  );
-}
+import React, {Component} from 'react'
 
+const App  = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {count: 0}
+  }
+  hundlePlusbutton = () => {
+    this.setState({count:this.state.count +1})
+  }
+
+  hundleminusbutton = () => {
+    this.setState({count:this.state.count -1})
+  }
+
+  render() {
+    return (
+    <React.Fragment>
+        <div>count : {this.state.count}</div>
+        <button onClick={this.hundlePlusbutton}>+1</button>
+        <button onClick={this.hundleminusbutton}>-1</button>
+     </React.Fragment>
+    )
+  }
+}
 export default App;
